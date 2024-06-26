deeb202432e718462d443d41abf1ea12d1f6b405
andresmuriel1 <tomanomm@hotmail.com>
2024-06-25 23:30:10 -0500

    Add files via upload

---
+++ CPR/index.js
@@ -0,0 +1,21 @@
+
+
+function aww() {
+    document.getElementById("subsbongo").innerHTML = "auuuuuuu!";
+}
+
+function miau() {
+    document.getElementById("subsjames").innerHTML = "miauuuuu!";
+}
+
+function oso() {
+    document.getElementById("subscayetano").innerHTML = "grrrrrrr!";
+}
+
+function vaca() {
+    document.getElementById("subsapolo").innerHTML = "muuuuuuu!";
+}
+
+function tato() {
+    document.getElementById("substato").innerHTML = "woooooof!";
+}
